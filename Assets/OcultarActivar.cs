5bffc0d3069a5960f42b786dc712a8913fe14271
jHONATAN190 <82833679+jHONATAN190@users.noreply.github.com>
2022-02-07 15:10:44 -0500

    hilfaaag

---
+++ Assets/OcultarActivar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcultarActivar : MonoBehaviour
+{
+    public GameObject objeto;
+    void Start()
+    {
+        
+    }
+
+    public void MostrarObjeto()
+
+    {
+        if (objeto.activeSelf == false)
+            objeto.SetActive(true);
+        else
+            objeto.SetActive(false);
+    }
+    
+}
